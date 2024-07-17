0e1d6f9f630ea6b597ecde42f58f6ec971e0ed5e
Alexander Pavliuk <yeeeyoooduck@gmail.com>
2024-07-17 23:02:14 +0400

    for review

---
+++ entry_level/agents/task1/agnt_task1.js
@@ -0,0 +1,24 @@
+﻿alert("Агент для установки случайных паролей запущен...");
+
+// Функция для назначения случайных паролей выбранным сотрудникам
+function assignRandomPasswords() {
+    var employeeIds = OBJECTS_ID_STR.split(';');
+
+    for (employeeId in employeeIds) {
+        var employeeDoc = tools.open_doc(OptInt(employeeId));
+        var newPassword = tools.random_string(8);
+
+        employeeDoc.TopElem.password = newPassword;
+        employeeDoc.Save();
+    }
+}
+
+// Запуск основной функции с обработкой ошибок
+try {
+    assignRandomPasswords();
+    alert("Пароли успешно назначены.");
+} catch (e) {
+    alert("Ошибка при назначении паролей: " + e.message);
+}
+
+alert("Агент для установки случайных паролей завершен.");
