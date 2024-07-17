064d4166adb050fb183d82ef19833fb5be1d6ba7
Alexander Pavliuk <101454081+yeeeyoooduck@users.noreply.github.com>
2024-07-17 08:14:37 +0400

    Delete entry_level/web_templates/task1 directory

--- entry_level/web_templates/task1/backend/gulpfile.ts
+++
@@ -1,7 +0,0 @@
-import { task } from "gulp";
-import { build } from "./gulp/tasks/build.task";
-import { dev } from "./gulp/tasks/dev.task";
-
-task("dev", dev);
-
-task("build", build);
--- entry_level/web_templates/task1/backend/src/modules/collaborators.ts
+++
@@ -1,5 +0,0 @@
-// =include ../utils/query.ts
-
-function getData() {
-	return selectAll("sql: SELECT * FROM collaborators")
-}
